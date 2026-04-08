502e61793b11590bc304d6a980bf9c8f350f0be4
RajanDz <dzaferadzovicrajan@gmail.com>
2026-04-08 21:59:29 +0200

    add temporary configuration for security

---
+++ src/main/java/dev_practice/cinema_backend/security/UserController.java
@@ -0,0 +1,16 @@
+package dev_practice.cinema_backend.security;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping
+    public String me(){
+        return "Test passed";
+    }
+}
---
+++ src/main/java/dev_practice/cinema_backend/security/WebConfig.java
@@ -0,0 +1,23 @@
+package dev_practice.cinema_backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+
+        return  httpSecurity.build();
+    }
+}
